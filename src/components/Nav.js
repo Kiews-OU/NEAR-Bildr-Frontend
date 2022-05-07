7fbe74c13e8e81bae7c8bf9ccd1538a566df2934
Biruk Erjamo <probiruk@gmail.com>
2022-05-07 15:25:38 +0300

    working on nav bar

---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Nav = () => {
+    return (
+        <div className='container-fluid shadow pop nav-header'>
+            <div className='row justify-content-between'>
+                <div className='col-md-3 p-1 d-flex align-items-center justify-content-center h-100'>
+                    <h4 className='text-center pop-bold mt-2'>Blidr</h4>
+                </div>
+                <div className='col-md-4 p-2 d-flex align-items-center justify-content-center h-100'>
+                    <input type="text" className="form-control" placeholder="Search" />
+                </div>
+                <div className='col-md-3 p-2 d-flex align-items-center justify-content-center h-100 nav-icon'><i class='bx bx-user'></i><div className='space-1'></div><i class='bx bx-log-out'></i></div>
+            </div>
+        </div >
+    )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Nav } from '../components/Nav'
 
 export const Home = () => {
     return (
-        <div>Home</div>
+        <>
+            <Nav />
+        </>
     )
 }
